ad96fdf696fab224b0316118fe9540ca876db7b4
Enderderder <richardwulansari@gmail.com>
2019-03-24 03:00:51 +1300

    ~ Add IDamageableObject interface ~ Add FlameBeast AI class inherited from AEnemyBase Class ~ Add 2 enemy ai task

---
+++ Source/Magic_n_Slash/Interfaces/DamageableObject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DamageableObject.h"
+
+// Add default functionality here for any IDamageableObject functions that are not pure virtual.
