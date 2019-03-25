d15523c9bbc09dad482987929ca641f910f24eb8
Enderderder <richardwulansari@gmail.com>
2019-03-25 18:17:10 +1300

    ~ FlameBeast behaviour (WIP)

---
+++ Source/Magic_n_Slash/FlameBeast.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "FlameBeast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGIC_N_SLASH_API AFlameBeast : public AEnemyBase
+{
+	GENERATED_BODY()
+	
+
+
+public:
+
+	AFlameBeast();
+};
