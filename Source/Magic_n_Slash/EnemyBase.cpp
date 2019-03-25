0e71bfe491f90cb6722a844a14b305e38cf49803
Enderderder <richardwulansari@gmail.com>
2019-03-25 23:27:34 +1300

    ~ Enemy multi point patrol finished

--- Source/Magic_n_Slash/EnemyBase.cpp
+++ Source/Magic_n_Slash/EnemyBase.cpp
@@ -10,11 +10,6 @@ AEnemyBase::AEnemyBase()
 
 }
 
-void AEnemyBase::ReceiveDamage_Implementation(float _value)
-{
-	//throw std::logic_error("The method or operation is not implemented.");
-}
-
 void AEnemyBase::GetActorEyesViewPoint(FVector& Location, FRotator& Rotation) const
 {
 	/// Not using parent class implementation
@@ -27,6 +22,11 @@ void AEnemyBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// Set default values
+	CurrHealth = MaxHealth;
+
+
+
 }
 
 void AEnemyBase::GetPerceptionLocRot_Implementation(FVector& Location, FRotator& Rotation) const
@@ -48,4 +48,3 @@ void AEnemyBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
-
