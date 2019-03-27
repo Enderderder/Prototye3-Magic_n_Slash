00913f6c92364229b2e5904cca100fbf0d8c7fdd
Enderderder <richardwulansari@gmail.com>
2019-03-28 00:21:35 +1300

    ~ Better procedural grass patch with collision detection

--- Source/Magic_n_Slash/BurnableGrassPatch.cpp
+++ Source/Magic_n_Slash/BurnableGrassPatch.cpp
@@ -8,18 +8,70 @@
 ABurnableGrassPatch::ABurnableGrassPatch()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	LotsOfGrassComponent = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("LotsOfGrass"));
 	RootComponent = LotsOfGrassComponent;
 
+	CylinderCollider = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CylinderCollider"));
+	CylinderCollider->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
 void ABurnableGrassPatch::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	// CONSTRUCTTTTTTTTTTTTTT!!!!!!!!!
+	GrassConstruction();
+}
+
+void ABurnableGrassPatch::GrassConstruction()
+{
+	FVector spawnLocation;
+	FVector spawnScale;
+	FTransform spawnTransform;
+
+	LotsOfGrassComponent->ClearInstances();
+	for (int32 i = 0; i < TotalSpawnCount; ++i)
+	{
+		spawnLocation = GetRandomPointInRadiusDistributed(SpawnRadius, SpawnRadiusDistribution);
+		spawnScale = GetRandomSize(MinHeight, MaxHeight, MinThickness, MaxThickness);
+		float randomRotationZ = FMath::FRandRange(0.0f, 360.0f);
+
+		spawnTransform.SetLocation(spawnLocation);
+		spawnTransform.SetScale3D(spawnScale);
+		spawnTransform.SetRotation(FQuat(FVector(0.0f, 0.0f, 1.0f), randomRotationZ));
+		
+		LotsOfGrassComponent->AddInstance(spawnTransform);
+	}
+}
+
+FVector ABurnableGrassPatch::GetRandomPointInRadiusDistributed(float _range, float _distribution)
+{
+	FVector resultVec;
+
+	float angle = FMath::FRandRange(0.0f, 360.0f);
+	float distribute = FMath::FRandRange(-_distribution, _distribution);
+	float distance = FMath::FRandRange(0.0f, _range + distribute);
+	float offsetX = distance * FMath::Cos(angle);
+	float offsetY = distance * FMath::Sin(angle);
+
+	resultVec = FVector(offsetX, offsetY, 0.0f);
+
+	return resultVec;
+}
+
+FVector ABurnableGrassPatch::GetRandomSize(float _minHeight, float _maxHeight, float _minThickness, float _maxThickness)
+{
+	FVector resultVec;
+
+	float thickness = FMath::FRandRange(_minThickness, _maxThickness);
+	float height = FMath::FRandRange(_minHeight, _maxHeight);
+
+	resultVec = FVector(thickness, thickness, height);
+
+	return resultVec;
 }
 
 // Called every frame
--- Source/Magic_n_Slash/BurnableGrassPatch.h
+++ Source/Magic_n_Slash/BurnableGrassPatch.h
@@ -16,13 +16,48 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	class UInstancedStaticMeshComponent* LotsOfGrassComponent;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* CylinderCollider;
+
 public:	
 	// Sets default values for this actor's properties
 	ABurnableGrassPatch();
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	int32 TotalSpawnCount;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float SpawnRadius;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float SpawnRadiusDistribution;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float MinHeight;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float MaxHeight;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float MinThickness;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Procedural Generation")
+	float MaxThickness;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	// Called to refresh the grass spawn
+	UFUNCTION(BlueprintCallable)
+	void GrassConstruction();
+
+	// Called to get a random point in radius with distribution
+	FVector GetRandomPointInRadiusDistributed(float _range, float _distribution);
+
+	// Called to get a random size of the grass
+	FVector GetRandomSize(float _minHeight, float _maxHeight, float _minThickness, float _maxThickness);
+
 
 public:	
 	// Called every frame
