6fd20caafece830d2027158bc477a2228cf7f3f1
James81919 <jjohnstone2520@gmail.com>
2019-03-28 19:01:55 +1300

    WARNING: DO NOT PULL! ~Added door shake ~Added translate to door ~Created interactiveMoveObject class [WIP]

---
+++ Source/Magic_n_Slash/InteractionMoveObject.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "InteractionMoveObject.h"
+#include "Runtime/Engine/Classes/Components/TimelineComponent.h"
+#include "InteractableObject.h"
+
+// Sets default values
+AInteractionMoveObject::AInteractionMoveObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractionMoveObject::BeginPlay()
+{
+	Super::BeginPlay();
+
+#pragma region SetupMoveTimeline
+	PrimaryActorTick.bCanEverTick = true;
+
+	MoveTimeLine = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline"));
+
+	InterpFunction.BindFunction(this, FName("OnTimelineUpdate"));
+	TimelineFinished.BindFunction(this, FName("OnTimelineFinished"));
+
+	if (MoveFloatCurve)
+	{
+		MoveTimeLine->AddInterpFloat(MoveFloatCurve, InterpFunction, FName("Alpha"));
+		MoveTimeLine->SetTimelineFinishedFunc(TimelineFinished);
+
+		startPosition = GetActorLocation();
+
+		SetTimelineValues();
+
+		MoveTimeLine->SetLooping(false);
+		MoveTimeLine->SetIgnoreTimeDilation(true);
+	}
+#pragma endregion
+}
+
+// Called every frame
+void AInteractionMoveObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractionMoveObject::MoveToOffsetPosition()
+{
+	Receive_MoveToOffsetPosition();
+
+	if (!IsActivated)
+	{
+		IsActivated = true;
+		MoveTimeLine->Play();
+	}
+}
+
+void AInteractionMoveObject::MoveToOriginalPosition()
+{
+	Recieve_MoveToOriginalPosition();
+
+	if (IsActivated)
+	{
+		IsActivated = false;
+		MoveTimeLine->Reverse();
+	}
+}
+
+void AInteractionMoveObject::CheckIsActivated()
+{
+	ConditionsMet = true;
+	
+	for (const TPair<AInteractableObject*, bool>& pair : InteractConditions)
+	{
+		if (pair.Key->IsActivated != pair.Value)
+		{
+			ConditionsMet = false;
+		}
+	}
+
+	if (ConditionsMet)
+	{
+		MoveToOffsetPosition();
+	}
+	else
+	{
+		MoveToOriginalPosition();
+	}
+}
+
+void AInteractionMoveObject::SetTimelineValues()
+{
+	endPosition = startPosition + MoveToOffset;
+
+	MoveTimeLine->SetPlayRate(1.0f / MoveTime);
+}
+
+void AInteractionMoveObject::OnTimelineUpdate(float _value)
+{
+	SetActorLocation(FMath::Lerp(startPosition, endPosition, _value));
+}
+
+void AInteractionMoveObject::OnTimelineFinished()
+{
+
+}
---
+++ Source/Magic_n_Slash/InteractionMoveObject.h
@@ -0,0 +1,84 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractionMoveObject.generated.h"
+
+UCLASS()
+class MAGIC_N_SLASH_API AInteractionMoveObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractionMoveObject();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Moving")
+	FVector MoveToOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Moving")
+	TMap<class AInteractableObject*, bool> InteractConditions;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Moving")
+	bool ConditionsMet;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Moving")
+	bool IsActivated;
+
+	UPROPERTY(EditAnywhere, Category = "Moving")
+	float MoveTime;
+
+#pragma region MoveTimelineVariables
+	UPROPERTY()
+	FVector startPosition;
+
+	UPROPERTY()
+	FVector endPosition;
+
+	UPROPERTY()
+	class UTimelineComponent* MoveTimeLine;
+
+	UPROPERTY(EditAnywhere, Category = "Moving: Timeline")
+	class UCurveFloat* MoveFloatCurve;
+
+	// Declare delegate function to be binded with TimelineFloatReturn(float _value)
+	FOnTimelineFloat InterpFunction{};
+
+	// Declare deletgate function to be binded with OnTimelineFinished()
+	FOnTimelineEvent TimelineFinished{};
+#pragma endregion
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void MoveToOffsetPosition();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Moving", meta = (DisplayName = "OnMoveToOffsetPosition"))
+	void Receive_MoveToOffsetPosition();
+
+	UFUNCTION()
+	void MoveToOriginalPosition();
+	UFUNCTION(BlueprintImplementableEvent, Category = "Moving", meta = (DisplayName = "OnMoveToOriginPosition"))
+	void Recieve_MoveToOriginalPosition();
+
+	UFUNCTION(BlueprintCallable)
+	void CheckIsActivated();
+
+#pragma region MoveTimelineFunctions
+	UFUNCTION(BlueprintCallable)
+	void SetTimelineValues();
+
+	UFUNCTION()
+	void OnTimelineUpdate(float _value);
+
+	UFUNCTION()
+	void OnTimelineFinished();
+#pragma endregion
+};
