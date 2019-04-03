18339963ae26dad16d2794e49605b33ba7b06ba8
Enderderder <richardwulansari@gmail.com>
2019-04-03 15:35:49 +1300

    ~ Tank beast started

---
+++ Source/Magic_n_Slash/TankBeast.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankBeast.h"
+
+ATankBeast::ATankBeast()
+{
+
+}
+
+void ATankBeast::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ATankBeast::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/Magic_n_Slash/TankBeast.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyBase.h"
+#include "TankBeast.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGIC_N_SLASH_API ATankBeast : public AEnemyBase
+{
+	GENERATED_BODY()
+
+
+public:
+	// Sets default values for this character's properties
+	ATankBeast();
+
+	// Combat sensors
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat | Sense")
+	float MeleeRange;
+
+protected:
+	// Called when the game starts or when spawnesd
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+
+};
